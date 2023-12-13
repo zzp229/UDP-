1643663de85f3c32f9069818ca39144c7fa8d42c
“nongnong” <a13556632276@gmail.com>
2023-12-13 15:28:09 +0800

    实现文字发送和接收

---
+++ 服务端/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 服务端
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UdpClient udpClient;
+        private const int listenPort = 11000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            udpClient = new UdpClient(11001); // 监听端口11001
+            Task.Run(() => ListenForMessages());
+        }
+
+
+        private void ListenForMessages()
+        {
+            try
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 11001);
+                while (true)
+                {
+                    byte[] bytes = udpClient.Receive(ref remoteEP);
+                    string receivedData = Encoding.UTF8.GetString(bytes); // 使用UTF-8解码
+
+                    // 使用UI线程
+                    Dispatcher.Invoke(() =>
+                    {
+                        // 更新UI显示接收到的消息
+                        messagesTextBox.Text += receivedData + Environment.NewLine;
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                // 处理异常
+            }
+        }
+
+    }
+}
---
+++ 用户端/用户端/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 用户端
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UdpClient udpClient;
+        private const int listenPort = 11000;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            udpClient = new UdpClient(listenPort);
+            
+        }
+
+        // 发送按钮的点击事件处理
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string message = messageTextBox.Text;
+                byte[] bytes = Encoding.UTF8.GetBytes(message); // 使用UTF-8编码
+                udpClient.Send(bytes, bytes.Length, "127.0.0.1", 11001);
+                // 更新UI或记录状态
+            }
+            catch (Exception ex)
+            {
+                // 处理异常
+            }
+        }
+
+
+    }
+}
